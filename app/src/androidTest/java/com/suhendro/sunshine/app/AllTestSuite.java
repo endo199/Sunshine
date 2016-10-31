e7b6549fb900fdeb586b35a4b1741e6a9a6ae6de
Suhendro <suhendro.mail@gmail.com>
2016-10-31 20:40:50 +0700

    optimize files/code based on code analytic

--- app/src/androidTest/java/com/suhendro/sunshine/app/AllTestSuite.java
+++ app/src/androidTest/java/com/suhendro/sunshine/app/AllTestSuite.java
@@ -9,10 +9,6 @@ import com.suhendro.sunshine.app.data.TestWeatherContract;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- * Created by Suhendro on 10/25/2016.
- */
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({TestDb.class, TestWeatherContract.class, TestUriMatcher.class, TestProvider.class, TestFetchWeatherTask.class})
 public class AllTestSuite {
